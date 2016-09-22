e2955090116fdfafdc206f0c200daabb658f1d69
Tatu Peltola <tatu.peltola@aalto.fi>
2016-09-23 02:55:18 +0300

    SPI works

--- stm32/dodekaedri.c
+++ stm32/dodekaedri.c
@@ -10,24 +10,48 @@ void SystemInit() {
 
 volatile int i;
 int main() {
-	//RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
 
-	GPIOA->MODER |= 1<<(PIN*2);
+	GPIO_Init(GPIOA, &(GPIO_InitTypeDef) {
+		.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7,
+		.GPIO_Mode = GPIO_Mode_AF,
+		.GPIO_Speed = GPIO_Speed_50MHz,
+		.GPIO_OType = GPIO_OType_PP,
+		.GPIO_PuPd = GPIO_PuPd_NOPULL
+	});
 
-	// TODO: try to make GPIO_Init work
 	/*GPIO_Init(GPIOA, &(GPIO_InitTypeDef) {
-		.GPIO_Pin = GPIO_Pin_5,
-		.GPIO_Mode = GPIO_Mode_OUT,
+		.GPIO_Pin = GPIO_Pin_7,
+		.GPIO_Mode = GPIO_Mode_AF,
+		.GPIO_Speed = GPIO_Speed_50MHz,
 		.GPIO_OType = GPIO_OType_PP,
 		.GPIO_PuPd = GPIO_PuPd_NOPULL
 	});*/
 
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
+
+	SPI_Init(SPI1, &(SPI_InitTypeDef) {
+		.SPI_Direction = SPI_Direction_1Line_Tx,
+		.SPI_Mode = SPI_Mode_Master,
+		.SPI_DataSize = SPI_DataSize_8b,
+		.SPI_CPOL = SPI_CPOL_Low,
+		.SPI_CPHA = SPI_CPHA_1Edge,
+		.SPI_NSS = SPI_NSS_Soft,
+		.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2,
+		.SPI_FirstBit = SPI_FirstBit_MSB,
+		.SPI_CRCPolynomial = 0
+	});
+
+	SPI_Cmd(SPI1, ENABLE);
+
 	for(;;) {
-		for(i = 0; i < 200000; i++) __asm("nop");
-		GPIOA->ODR = 1<<PIN;
-		for(i = 0; i < 200000; i++) __asm("nop");
-		GPIOA->ODR = 0;
+		for(i = 0; i < 10000; i++) __asm("nop");
+		SPI_I2S_SendData(SPI1, 0x55);
+		for(i = 0; i < 10000; i++) __asm("nop");
+		SPI_I2S_SendData(SPI1, 0xAA);
 	}
 	return 0;
 }
