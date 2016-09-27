16ba1b308ff866b1f25a856ff7c284ba108b31ec
Tatu Peltola <tatu.peltola@aalto.fi>
2016-09-27 23:55:50 +0300

    Use external clock

--- stm32/dodekaedri.c
+++ stm32/dodekaedri.c
@@ -95,6 +95,22 @@ int main() {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
+	// bit banged I2C pins
+	GPIO_Init(GPIOB, &(GPIO_InitTypeDef) {
+		.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9,
+		.GPIO_Mode = GPIO_Mode_OUT,
+		.GPIO_Speed = GPIO_Speed_50MHz,
+		.GPIO_OType = GPIO_OType_OD,
+		.GPIO_PuPd = GPIO_PuPd_UP
+	});
+
+	// init SI5351
+	int reg_i;
+	for(reg_i = 0; reg_i < NUM_REGS_MAX; reg_i++) {
+		writereg(a, Reg_Store[reg_i].Reg_Addr, Reg_Store[reg_i].Reg_Val);
+	}
+
+
 	// USART1 initialization
 	// External USB-UART converter is connected to PA9
 	// because the built-in converter on Nucleo board in USART2
@@ -147,29 +163,26 @@ int main() {
 	});
 	SPI_Cmd(SPI1, ENABLE);
 
-
-	// bit banged I2C pins
-	GPIO_Init(GPIOB, &(GPIO_InitTypeDef) {
-		.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9,
-		.GPIO_Mode = GPIO_Mode_OUT,
-		.GPIO_Speed = GPIO_Speed_50MHz,
-		.GPIO_OType = GPIO_OType_OD,
-		.GPIO_PuPd = GPIO_PuPd_UP
-	});
-
-
 	// I2S PLL initialization
 	RCC_PLLI2SCmd(DISABLE); // PLLI2SConfig should be called with PLLI2S disabled
 
 	// PLLI2S clock source is common with main PLL configured with RCC_PLLConfig.
-	// HSI (internal RC oscillator) is now used for initial tests.
-	RCC_PLLConfig(RCC_PLLSource_HSI, 8, 50, 2, 4);
+	// Using 26 MHz clock input (HSE)
+	RCC_HSEConfig(RCC_HSE_Bypass);
+	print("Waiting for HSE\r\n");
+	RCC_WaitForHSEStartUp();
+	print("HSE started up\r\n");
+
+	RCC_PLLConfig(RCC_PLLSource_HSE, 13, 50, 2, 4);
 	RCC_PLLCmd(ENABLE);
 
 	RCC_I2SCLKConfig(RCC_I2S2CLKSource_PLLI2S);
 	RCC_PLLI2SConfig(72,3);
 	RCC_PLLI2SCmd(ENABLE);
+
+	print("Waiting for PLLI2S\r\n");
 	while(!RCC_GetFlagStatus(RCC_FLAG_PLLI2SRDY));
+	print("PLLI2S ready\r\n");
 
 	// I2S2 initialization
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE); // I2S2
@@ -227,15 +240,10 @@ int main() {
 
 
 
-	// init SI5351
-	int reg_i;
-	for(reg_i = 0; reg_i < NUM_REGS_MAX; reg_i++) {
-		writereg(a, Reg_Store[reg_i].Reg_Addr, Reg_Store[reg_i].Reg_Val);
-	}
 
 	uint16_t audio = 0;
 	for(;;) {
-		print("a");
+		//print("a");
 		while(!SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_TXE));
 		SPI_I2S_SendData(SPI2, audio);
 		SPI_I2S_SendData(SPI3, -audio);
